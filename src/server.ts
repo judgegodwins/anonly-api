8aa402bcff88f154b457514ca56eb9a96350ebe6
Judge <judgegodwins@gmail.com>
2021-09-02 13:47:35 +0100

    remove console.logs

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { port } from './config';
 
 app
   .listen(port, () => {
-    console.log('here')
     Logger.info(`server running on port ${port}`)
   })
   .on("error", (e) => Logger.error(e));
