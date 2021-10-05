034a43aad3da1b2d24c907ef492ba885925fbb02
Judge <judgegodwins@gmail.com>
2021-10-05 05:38:49 +0100

    pick selected fields to encode

--- src/auth/authUtils.ts
+++ src/auth/authUtils.ts
@@ -2,12 +2,13 @@ import { jwtSecret } from '../config';
 import User from '../database/models/User';
 import jwt, { Secret } from 'jsonwebtoken';
 import { AuthFailureError } from '../core/ApiError';
+import _ from 'lodash';
 
 export const createToken = (user: User): { accessToken: string, expiresOn: string } => {
   const expiresOn = new Date(Date.now() + 1000 * 60 * 60 * 24 * 15).toISOString()
 
   const accessToken = jwt.sign(
-    user,
+    _.pick(user, ["_id", "username", "email", "roles", "verified"]),
     jwtSecret as Secret,
     { expiresIn: '10d' }
   )
